98b42483a02117fc11767f6341c486d4ce988342
fer967 <127159162+fer967@users.noreply.github.com>
2023-04-12 20:33:30 -0300

    Add files via upload

---
+++ MainTrabajoPractico.java
@@ -0,0 +1,13 @@
+package gabriel;
+
+import java.nio.file.Paths;
+
+public class MainTrabajoPractico {
+
+	public static void main(String[] args) {
+		Paths.get("src/test/resources/resultados_test1.csv");
+		
+	}                                                                 //LEER ARCHIVO CSV RESULTADOS
+
+  }                                                                   //LEER ARCHIVO CSV PRONOSTICO                                                         
+                                                                      //CALCULAR Y MOSTRAR PUNTOS
---
+++ Pronosticos.java
@@ -0,0 +1,12 @@
+package gabriel;
+
+public class Pronosticos {
+
+	private Partido partido;
+	private Equipo equipo;
+	private String resultado;
+	
+	
+	
+	
+}
